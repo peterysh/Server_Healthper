9a37f7a42dbf125b53988753aa0d6b0717b988c3
peterysh <dodo7se@gmail.com>
2022-07-25 14:23:44 +0900

    fetch: session login

--- healthper/src/main/java/umc/healthper/api/LoginController.java
+++ healthper/src/main/java/umc/healthper/api/LoginController.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import umc.healthper.domain.login.LoginService;
+import umc.healthper.domain.login.SessionConst;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @Slf4j
@@ -18,9 +22,15 @@ public class LoginController {
 
     @GetMapping("/login")
     @ResponseBody
-    public String login(@RequestParam("code")String code) throws JsonProcessingException {
-        Long kakaoId = loginService.kakoLogin(code);
+    public String login(@RequestParam("code")String code, HttpServletRequest request) throws JsonProcessingException {
+        Long loginMember = loginService.kakoLogin(code);
+        if(loginMember == null){
+            return "failed";
+        }
+        HttpSession session = request.getSession();
+        //세션에 로그인 회원 정보 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
 
-        return "ok " + kakaoId;
+        return "ok " + session.getAttribute(SessionConst.LOGIN_MEMBER);
     }
 }
---
+++ healthper/src/main/java/umc/healthper/domain/login/SessionConst.java
@@ -0,0 +1,5 @@
+package umc.healthper.domain.login;
+
+public interface SessionConst {
+    String LOGIN_MEMBER = "loginMember";
+}
